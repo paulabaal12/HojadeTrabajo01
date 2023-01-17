d832b260b59deff85f54cfada29acc7c351f4c69
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-01-16 20:19:02 -0600

    Start DP

---
+++ DriverProgram.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+
+/**
+ * @author fabi
+ *
+ */
+public class DriverProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("  ██████████████████████████████████████████████");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░¿Deseas prender la radio?░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░Si (1)░░░░░No (2)░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println(" ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████");
+		System.out.println("█▀████████████████████████████████████████████████");
+		System.out.println("█▄▄██▌                                      ███░▐█");
+		System.out.println("▀███▀      █▄▄▄▄█    ██▄▄██    █▄▄▄▄█       ▀███▀");
+		System.out.println("           ██████     ████     ██████             ");
+
+
+	}
+
+
+}
