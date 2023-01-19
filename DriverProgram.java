24004333e5f167755d5543e266ac60678e3c4fd6
Fabiola <105880646+FabiDeG@users.noreply.github.com>
2023-01-18 20:17:59 -0600

    Case 2 and 3 for DP

--- DriverProgram.java
+++ DriverProgram.java
@@ -62,7 +62,7 @@ public class DriverProgram {
 				
 				switch (respon) {
 				case 1:{
-				
+					MenuR();
 				}break;
 				
 				case 2:{
@@ -96,5 +96,140 @@ public class DriverProgram {
 
 		}
 
+	public static void MenuR() {
+		
+		int opt = 0;
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("  ██████████████████████████████████████████████");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░Bienvenidos a tu radio favorita░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░Que deseas hacer?░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░1 Cambiar frecuencia░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░2. Cambiar emisora░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░3. Guardar emisora░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░4. Seleccionar botón░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println("  █░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░█");
+		System.out.println(" ████░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░████");
+		System.out.println("█▀████████████████████████████████████████████████");
+		System.out.println("█▄▄██▌                                      ███░▐█");
+		System.out.println("▀███▀      █▄▄▄▄█    ██▄▄██    █▄▄▄▄█       ▀███▀");
+		System.out.println("           ██████     ████     ██████             ");
+		
+		opt = Integer.parseInt(in.nextLine());
+		do {
+		switch (opt) {
+			case 1:{
+				
+				System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░La frecuencia ha sido cambiada░░░░░░▓▓");
+				System.out.println("▓▓░░░░¿La frecuencia actual es FM?: ░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░False = AM░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░True = FM░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+				System.out.println("");
+				System.out.println("");
+				
+				MenuR();
+				
+			}break;
+			
+			case 2:{
+				System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░¿Deseas subir la emisora? (1)░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░o░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░¿Deseas bajar la emisora? (2)░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+				System.out.println("");
+				System.out.println("");	
+				
+				int res = 0;
+				res = Integer.parseInt(in.nextLine());
+				
+				if (res == 1){
+					System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░La emisora actual es: ░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+					System.out.println("");
+					System.out.println("");
+					MenuR();
+				}
+				
+				if (res == 2){
+					System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░La emisora actual es:░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+					System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+					System.out.println("");
+					System.out.println("");
+					MenuR();
+				}
+			}break;
+			
+			case 3:{
+				System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░La emisora ha sido guardada con éxito░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓");
+				System.out.println("▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+				System.out.println("");
+				System.out.println("");
+				MenuR();
+			}break;
+			
+			default:{
+				System.out.println();
+			}break;
+			
+			}
+		}while(opt != 10);
+		
+		
+	}
 
 }
